64153d5d8f135c9e9fada6f6720a3a7d18ce3125
Gamddalki <hannahk01@naver.com>
2022-02-22 00:17:03 +0900

    Create rootController.js

---
+++ src/controllers/rootController.js
@@ -0,0 +1,13 @@
+export const home = (req, res) => res.render("home");
+
+export const etoe = (req, res) => res.render("./etoe/etoe");
+
+export const solution = (req, res) => res.render("./solution/solution");
+
+export const cloud = (req, res) => res.render("./cloud/cloud");
+
+export const contact = (req, res) => res.render("contact");
+
+export const career = (req, res) => res.render("./career/career");
+
+export const search = (req, res) => res.render("search");
