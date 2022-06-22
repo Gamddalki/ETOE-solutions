2cb6f99ac298e36411c9a2293c54612ddb21a30d
Gamddalki <hannahk01@naver.com>
2022-06-22 14:40:35 +0900

    npm i morgan

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 8000;
 const app = express();
+const logger = morgan("dev");
+
+app.use(logger);
 
 const handleListening = () =>
   console.log(`Server Running!😊 🤍 http://localhost:${PORT} 🤍`);
