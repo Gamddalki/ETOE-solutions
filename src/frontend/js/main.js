558cc726fa19d7607c08e6130fd21a689040d90c
Gamddalki <hannahk01@naver.com>
2022-07-07 19:01:31 +0900

    Navigation bar animating

--- src/frontend/js/main.js
+++ src/frontend/js/main.js
@@ -1,1 +1,12 @@
 import "../scss/styles.scss";
+
+$(document).ready(function () {
+  $(".header__contents-list>li").mouseover(function () {
+    $(this).children(".list_sub").stop().slideDown();
+    $(".list_main").not(this).css("color", "#aaaaaa");
+  });
+  $(".header__contents-list>li").mouseleave(function () {
+    $(this).children(".list_sub").stop().slideUp();
+    $(".list_main").css("color", "#111");
+  });
+});
