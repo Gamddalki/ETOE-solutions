e4347826887a7f6ce366005bcdccd242ad1a79da
Gamddalki <hannahk01@naver.com>
2022-08-02 00:31:45 +0900

    Update rootController.js

--- src/controllers/rootController.js
+++ src/controllers/rootController.js
@@ -13,8 +13,6 @@ export const service = (req, res) => res.render("./cloud/service");
 export const contact = (req, res) => res.render("contact");
 
 export const career = (req, res) => res.render("./career/career");
-export const education = (req, res) => res.render("./career/education");
-export const jobs = (req, res) => res.render("./career/jobs");
 
 export const sitemap = (req, res) => res.render("sitemap");
 export const terms = (req, res) => res.render("terms");
