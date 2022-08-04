2008db37664a1572d10e5fb39c296a5af5bbeb5b
Gamddalki <hannahk01@naver.com>
2022-08-04 18:52:56 +0900

    Update main.js

--- src/frontend/js/main.js
+++ src/frontend/js/main.js
@@ -3,7 +3,7 @@ import "../scss/styles.scss";
 import "../img/logo.png";
 import "../img/main-background.png";
 
-$(document).ready(function () {
+(function ($) {
   $(".header__contents-list>li").mouseover(function () {
     $(this).children(".list_sub").stop().slideDown();
     $(".list_main").not(this).css("color", "#aaaaaa");
@@ -12,4 +12,4 @@ $(document).ready(function () {
     $(this).children(".list_sub").stop().slideUp();
     $(".list_main").css("color", "#111");
   });
-});
+})(jQuery);
