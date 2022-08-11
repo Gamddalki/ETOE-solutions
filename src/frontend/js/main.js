0a229e92b84cc782af8e857b59f0c6b4b27a61b5
Gamddalki <hannahk01@naver.com>
2022-08-11 18:12:46 +0900

    Update main.js

--- src/frontend/js/main.js
+++ src/frontend/js/main.js
@@ -1,8 +1,11 @@
 import "../scss/styles.scss";
 
 import "../img/logo.png";
-import "../img/main-background.png";
-import "../img/main-background.webm";
+import "../img/main-background.mp4";
+import "../img/customer.jpg";
+import "../img/solution.jpg";
+import "../img/service.jpg";
+import "../img/consulting.jpg";
 
 (function ($) {
   $(".header__contents-list>li").mouseover(function () {
