cba05f0e641da7790bef005fc4905265523c2408
Gamddalki <hannahk01@naver.com>
2025-07-02 18:59:52 +0900

    🛠️ build: Initialize default CSS

--- etoesolutions/src/index.js
+++ etoesolutions/src/index.js
@@ -2,9 +2,127 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+@font-face {
+    font-family: 'Paperlogy-8ExtraBold';
+    src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/2408-3@1.0/Paperlogy-8ExtraBold.woff2') format('woff2');
+    font-weight: 800;
+    font-style: normal;
+}
+@font-face {
+    font-family: 'Paperlogy-4Regular';
+    src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/2408-3@1.0/Paperlogy-4Regular.woff2') format('woff2');
+    font-weight: 400;
+    font-style: normal;
+}
+@font-face {
+    font-family: 'GmarketSansMedium';
+    src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansMedium.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+@font-face {
+    font-family: 'GmarketSansLight';
+    src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansLight.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+	display: block;
+}
+body {
+	line-height: 1;
+}
+ol, ul {
+	list-style: none;
+}
+blockquote, q {
+	quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}
+*{
+  box-sizing: border-box;
+  font-family: "GmarketSansLight", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  font-weight: 300;
+  line-height: 1.2;
+  color: white;
+}
+a{
+  text-decoration: none;
+  color: inherit;
+}
+input, select, textarea {
+    border: none;
+    outline: none;
+    background-color: transparent;
+  }
+button {
+    cursor: pointer;
+    border: none;
+    background: none;
+  }
+h1 {
+  font-family: 'Paperlogy-8ExtraBold', -apple-system, sans-serif;
+  color: white;
+  line-height: 35px;
+  font-size: 25px;
+}
+h2 {
+  font-family: 'Paperlogy-4Regular', -apple-system, sans-serif;
+  color: white;
+  line-height: 35px;
+  font-size: 25px;
+}
+h3 {
+  font-family: 'GmarketSansMedium', -apple-system, sans-serif;
+  color: white;
+  line-height: 20px;
+  font-size: 15px;
+}
+span{
+  font-family: 'GmarketSansLight', -apple-system, sans-serif;
+  color: white;
+  line-height: 20px;
+  font-size: 15px;
+}
+`;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
