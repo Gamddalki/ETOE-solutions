cc347d026ba394d25e82a8c739c77880bb14c646
Gamddalki <hannahk01@naver.com>
2025-07-02 18:57:28 +0900

    🛠️ build: Initialize project using Create React App

--- src/routers/solutionRouter.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-import { consulting } from "../controllers/rootController";
-
-const solutionRouter = express.Router();
-
-solutionRouter.get("/consulting", consulting);
-
-export default solutionRouter;
