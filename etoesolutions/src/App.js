ed0dc18a10df52406c882e4bca6491c65ff6a9b0
Gamddalki <hannahk01@naver.com>
2025-07-07 16:44:58 +0900

    ✨ feat : add theme & routes

--- etoesolutions/src/App.js
+++ etoesolutions/src/App.js
@@ -1,5 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import theme from "./theme";
+import Header from "./Components/Header";
+import Home from "./Routes/Home";
+import Footer from "./Components/Footer";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
