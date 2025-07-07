4cf30f630a1a716e992ae8df5fa974486ee62d77
Gamddalki <hannahk01@naver.com>
2025-07-07 16:41:42 +0900

    💄 UI : add theme.js

---
+++ etoesolutions/src/theme.js
@@ -0,0 +1,21 @@
+const theme = {
+  colors: {
+    background: "#FDFFF5",
+    primary: "#1A3C6F",
+    hover: "#f0f0f0",
+    header: "white",
+    headerText: "black",
+    headerSubText: "#444",
+    secondary: "#326297",
+    footer: "#1A3C6F",
+    footerText: "white",
+    overlay: "rgba(0,0,0,0.7)",
+  },
+  breakpoints: {
+    desktop: "1050px",
+    tablet: "768px",
+    mobile: "480px",
+  },
+};
+
+export default theme;
